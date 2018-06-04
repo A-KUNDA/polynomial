c1724e6adf5c95d56de6ad33d4dfdb419031cdb6
Ashok Kunda <ashokkunda@gmail.com>
2018-06-03 21:33:19 -0700

    added add and subtract functionality to polynomials

--- polynomial.py
+++ polynomial.py
@@ -7,7 +7,9 @@ class Polynomial(object):
 	def __repr__(self):
 		s = ""
 		for deg, c in enumerate(self.coeff):
-			s += str(c) + "x" + "^" + str(deg)
+			if c != 1:
+				s += str(c)
+			s += "x" + "^" + str(deg)
 			if deg != len(self.coeff) - 1:
 				s += " + "
 		return "Polynomial: " + s
@@ -15,11 +17,24 @@ class Polynomial(object):
 	def degree(self):
 		return len(self.coeff) - 1
 
-	def eval(self, num):
+	def __call__(self, num):
 		res = 0
 		for deg, coeff in enumerate(self.coeff):
 			res += coeff * num**deg
 		return res
 
+	def __add__(self, other):
+		coeff1 = self.coeff
+		coeff2 = other.coeff
+		result = [a + b for a, b in zip(coeff1, coeff2)]
+		return Polynomial(*result)
+
+	def __sub__(self, other):
+		coeff1 = self.coeff
+		coeff2 = other.coeff
+		result = [a - b for a, b in zip(coeff1, coeff2)]
+		return Polynomial(*result)
+
 f = Polynomial(1, 1, 41)
-print(f)
+g = Polynomial(1, 2, 3)
+print(f(2))
