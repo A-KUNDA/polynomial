2c1565c15481c315ba3a82682f491cd3ee0ccfb3
Ashok Kunda <ashokkunda@gmail.com>
2018-06-18 20:47:21 -0700

    rebuilt lexer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from parse import lex
+
 if __name__ == "__main__":
 	s = ""
 	print("Type 'exit' to stop")
 	while (s.lower() != "exit"):
 		s = input("> ")
+		lex(s)
--- parse.py
+++ parse.py
@@ -1,8 +1,45 @@
-from io import BytesIO
-from tokenize import tokenize
-# change this to smaller custom files later
+import sys
+from re import compile
+
+RESERVED = "RESERVED"
+NUM = "NUM"
+ID = "ID"
+
+token_exprs = [
+	(r'\s+',                   None),
+	(r'=',                     RESERVED),
+	(r'\(',                    RESERVED),
+	(r'\)',                    RESERVED),
+	(r'\+',                    RESERVED),
+	(r'-',                     RESERVED),
+	(r'\*',                    RESERVED),
+	(r'/',                     RESERVED),
+	(r'\d+(\.\d+)?',           NUM),
+	(r'\w',                    ID),
+]
 
 def lex(expr):
-	t = tokenize(BytesIO(expr.encode('utf-8')).readline)
-	tokens = [token[1] for token in t if token[1]]
-	print(tokens)
+	return lex_e(expr, token_exprs)
+
+def lex_e(expr, token_exprs):
+	pos = 0
+	tokens = []
+	while pos < len(expr):
+		match = None
+		for token_expr in token_exprs:
+			pattern, tag = token_expr
+			regex = compile(pattern)
+			match = regex.match(expr, pos)
+			if match:
+				text = match.group(0)
+				if tag:
+					token = (text, tag)
+					tokens.append(token)
+				break
+		if not match:
+			sys.stderr.write("Illegal character: " + expr[pos] + "\n")
+			sys.exit(1)
+		else:
+			pos = match.end(0)
+	#print(tokens)
+	return tokens
